bc6595e1717185bb55a3a029532bb3048632390d
marin <marin@LAPTOP-GKHAG6BP>
2019-04-24 21:42:43 -0300

    ajustes Aplicativo

---
+++ ps2/src/aplicativo/Aplicativo.java
@@ -0,0 +1,40 @@
+package aplicativo;
+
+public class Aplicativo {
+	long id;
+	String nome;
+	String desenvolvedor;
+	int numDownloads;
+	
+	public long getId() {
+		return this.id;
+	}
+	
+	public void setId(long id) {
+		this.id= id;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getDesenvolvedor() {
+		return this.desenvolvedor;
+	}
+	
+	public void setDesenvolvedor(String desenvolvedor) {
+		this.desenvolvedor = desenvolvedor;
+	}
+	
+	public int getNumDownloads() {
+		return numDownloads;
+	}
+	
+	public void setNumDownLoads(int num) {
+		this.numDownloads = num;
+	}
+}
