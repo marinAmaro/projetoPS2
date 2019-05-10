5271b8686db26af4a7d839aabb64161c4c11a961
marina <marin@LAPTOP-GKHAG6BP>
2019-05-10 00:47:53 -0300

    Caminho feliz de todos os CRUD de 'APLICAÇÃO'

--- ps2/src/services/Main.java
+++ ps2/src/services/Main.java
@@ -10,19 +10,41 @@ public class Main {
 	public static void main(String [] args) throws Exception {
 		
 		ConexaoJDBC conexao = new ConexaoJDBC("root", "projeto", "localhost", 3306, "ps2");
+		
+		//TESTE APLICATIVO ---- READ
 		AplicativoDAO dao = new AplicativoDAO(conexao);
-
 		List<Aplicativo> aplicativo = dao.read();
 
-		System.out.println("LISTA DE aplicativo:");
+		System.out.println("LISTA DE APLICATIVO:: ");
 		for (int i = 0; i<aplicativo.size(); i++) {
 			Aplicativo p = (Aplicativo) aplicativo.get(i);
 			System.out.println("* " + p.getNome());
 	}
+				
+		//TESTE APLICATIVO ---- CREATE
+		Aplicativo appCriar= new Aplicativo();
+		appCriar.setDesenvolvedor("Marina teste");
+		appCriar.setNumDownLoads(550);
+		appCriar.setNome("CRUD");
+		
+		
+		System.out.println("CRIAÇÃO APLICATIVO:: ");
+		Aplicativo appCriado = dao.create(appCriar);
+		System.out.println("ID:: " + appCriado.getId());
+		
+		//TESTE APLICATIVO ---- UPDATE
+		appCriado.setNumDownLoads(1573);
+		int nUpdates = dao.update(appCriado);
+		System.out.println("nUpdates->> " + nUpdates);
+		
+		
+		//TESTE APLICATIVO ---- DELETE
+		long id = appCriado.getId();
+		int deletados = dao.delete(id);
+		System.out.println("numero de deleados:: " + deletados);
 		
 		//IMPORTANTE!!!!!!!
 		dao.close();
 		conexao.close();
-				
 	}
 }
